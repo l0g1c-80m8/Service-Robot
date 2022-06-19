234857644ee5c2f5d4127eee8c869297c2cbf41d
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-06-19 03:01:22 -0700

    remove file clutter

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -14,9 +14,9 @@ xterm -e " source devel/setup.bash; roslaunch my_robot slam_gmapping.launch " &
 sleep 5
 
 # Launch rviz for navigation
-xterm -e " source devel/setup.bash; roslaunch my_robot view_navigation.launch rviz_config:=\"./rvizConfig/viz-test-slam.rviz\"" &
+xterm -e " source devel/setup.bash; roslaunch my_robot view_navigation.launch " &
 
 sleep 5
 
 # Launch teleop node to manually control the robot
-xterm -e " source devel/setup.bash; roslaunch my_robot keyboard_teleop.launch " &
+xterm -e " source devel/setup.bash; rosrun teleop_twist_keyboard teleop_twist_keyboard.py " &
