fe7814a66328c3ee025519114a0fc031aba98592
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-06-19 04:53:22 -0700

    tuned additional params

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -11,7 +11,12 @@ sleep 10
 # Launch amcl package for localization
 xterm -e " source devel/setup.bash; roslaunch my_robot amcl.launch " &
 
-sleep 10
+sleep 5
 
 # Launch rviz to localize the results
 xterm -e " source devel/setup.bash; roslaunch my_robot view_navigation.launch " &
+
+# sleep 5
+
+# # Launch rqt
+# xterm -e " rosrun rqt_graph rqt_graph " &
