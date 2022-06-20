90115749ddcbadbec9f0d9207207e218dbcf501e
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-06-20 23:55:19 +0530

    Refactor pick_objects.sh script

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
+# Move to catkin_ws root
 cd ../../ 
 
+# Launch the world containing the robot
 xterm -e " source devel/setup.bash; roslaunch my_robot world.launch " &
 
 sleep 10
 
+# Launch amcl package for localization
 xterm -e " source devel/setup.bash; roslaunch my_robot amcl.launch " &
 
 sleep 5
 
-xterm -e " source devel/setup.bash; roslaunch my_robot navigation.launch " &
+# Launch rviz to localize the results
+xterm -e " source devel/setup.bash; roslaunch my_robot view_navigation.launch " &
 
 sleep 5
 
-xterm -e " rosrun rviz rviz " & 
-
-sleep 5
-
+# Launch pick_object to guide robot across the map
 xterm -e " source devel/setup.bash; roslaunch my_robot pick_objects.launch " &
